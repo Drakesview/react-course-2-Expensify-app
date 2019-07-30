b7b8ab86601f2608c7ecc32d6e88ec6a7d0f7a0a
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-07-30 13:15:07 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 //entry -> output
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-console.log()
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
 
-module.exports = {
-    entry: './src/app.js',
+    return {
+        entry: './src/app.js',
     output: {
         path: path.join(__dirname,'public') ,
         filename: 'bundle.js'
     },
+    mode: isProduction ? 'production' : 'development',
     module: {
         rules: [{
             loader:'babel-loader',
@@ -17,15 +21,30 @@ module.exports = {
         }, {
             test:/\.s?css$/,
             use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+                {
+                loader:MiniCssExtractPlugin.loader
+                }, {
+                    loader:'css-loader',
+                    options: {
+                        sourceMap:true
+                    }
+                }, {
+                    loader:'sass-loader',
+                    options: {
+                        sourceMap:true
+                    }
+                }
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase:path.join(__dirname,'public'),
         historyApiFallback: true
     }
+}
+
 };
