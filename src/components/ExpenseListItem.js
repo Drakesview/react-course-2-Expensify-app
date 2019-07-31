2adf515ff49e2acbb1346ba48e538f0a9e91a4d6
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-07-31 09:31:02 +0100

    Setup formmating for amount to use UK symbols

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,9 +8,23 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
        <Link to={'/edit/'+id}> <h3>{description}</h3> </Link>
         <p>
         {numeral(amount / 100).format('$0,0.00')}
-         -
+         - 
         {moment(createdAt).format('Do MMMM YYYY')}</p>
     </div>
 )
+
+
+//load a local for numeral
+numeral.register('locale', 'uk', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    currency: {
+        symbol: '£'
+    }
+});
+
+numeral.locale('uk');
 
 export default ExpenseListItem
