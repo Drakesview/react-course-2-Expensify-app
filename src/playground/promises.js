3dd03fdc5bf9bc78947e2f40cf0bd85cf02093a6
Adam Shenton <shenton_a@wmsldom.wmsl.co.uk>
2019-09-05 11:44:07 +0100

    Setup test database enviroment

---
+++ src/playground/promises.js
@@ -0,0 +1,16 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        // resolve('this is my resolved data')
+        reject('Something went wrong');
+    },5000);
+});
+console.log('before')
+promise.then((data) => {
+    console.log('1',data)
+}).catch((error) => {
+    console.log(error)
+});
+
+
+
+console.log('after')
